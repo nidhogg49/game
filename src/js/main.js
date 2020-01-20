67a9ee412ef05e028e2c5efe8d62a76752ae4be2
nidhogg49 <nidhogg49@gmail.com>
2020-01-20 04:47:16 +0300

    game upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,80 +1,182 @@
-'use strict';
+$(document).ready(function(){
+    var $slider = $('.slider');
+    var game = {
+        name: '',
+        surname: '',
+        questions: [
+            {
+                result: null,
+                correctText: 'Верно!<br/>Вы на правильном пути.',
+                wrongText: 'Не верно.<br/>За готовой картой Клиенту надо будет приехать в офис.',
+                imgCorrect: 'url(src/img/1_2.png)',
+                imgWrong: 'url(src/img/1_1.png)',
+            },
+            {
+                result: null,
+                correctText: 'Правильно!',
+                wrongText: 'Нет, заявок подано в два раза больше',
+                imgCorrect: 'url(src/img/2_2.png)',
+                imgWrong: 'url(src/img/2_1.png)',
+            },
+            {
+                result: null,
+                correctText: 'Правильно!',
+                wrongText: 'Не верно',
+                imgCorrect: 'url(src/img/1_2.png)',
+                imgWrong: 'url(src/img/1_1.png)',
+            },
+            {
+                result: null,
+                correctText: 'Верно!<br/>Сейчас только 24%, но мы уверены, что общими усилиями их доля увеличится!',
+                wrongText: 'Хотелось бы, но нет!<br/>Функционал реализован для гарантий в рамках ФЗ.',
+                imgCorrect: 'url(src/img/2_2.png)',
+                imgWrong: 'url(src/img/2_1.png)',
+            },
+            {
+                result: null,
+                correctText: 'Верно!',
+                wrongText: 'У вас еще не было сделок по Белой зоне?<br/>Так будут!',
+                imgCorrect: 'url(src/img/1_2.png)',
+                imgWrong: 'url(src/img/1_1.png)',
+            },
+            {
+                result: null,
+                correctText: 'Верно, теперь СББОЛ стал еще оперативнее!',
+                wrongText: 'Можно и так, но зачем тратить время, если СМС Клиенту поступит быстрее!',
+                imgCorrect: 'url(src/img/2_2.png)',
+                imgWrong: 'url(src/img/2_1.png)',
+            },
+            {
+                result: null,
+                correctText: 'Верно!',
+                wrongText: '',
+                imgCorrect: 'url(src/img/1_2.png)',
+                imgWrong: 'url(src/img/1_1.png)',
+            }, 
+        ],
+        current: 1 //:TODO
+    };
 
-function parallax(){
-    var scrolled = window.pageYOffset,
-        elem = document.querySelector('.header__text'),
-        elemOffsetTop = elem.getBoundingClientRect().top;
+    localStorage.clear();
 
-    if (elemOffsetTop > 0) {
-        elem.style['transform'] = 'translateY('+ (-scrolled * 1) +'px)';
-    }
-}
-
-function navigationfix(){
-    var h = document.querySelector('#intro').offsetHeight;
-
-    if(window.pageYOffset > h){
-        document.querySelector('.nav').classList.add('nav_fixed');
-    }
-    else if (window.pageYOffset < h){
-        document.querySelector('.nav').classList.remove('nav_fixed');
-    }
-}
-
-window.addEventListener('scroll', function() {
-    parallax();
-    navigationfix();
-});
-
-function scroll(to, duration){
-    if (duration <= 0) {return;}
-
-    var element = document.body,
-        difference = to - element.scrollTop,
-        perTick = difference / duration * 10;
-
-    setTimeout(function() {
-        element.scrollTop = element.scrollTop + perTick;
-        if (element.scrollTop == to) return;
-        scroll(to, duration - 10);
-    }, 10);
-}
-
-function toggleClass(el, myClass) {
-    if (el && el.className && el.className.indexOf(myClass) >= 0) {
-        var pattern = new RegExp('\\s*' + myClass + '\\s*');
-
-        el.className = el.className.replace(pattern, ' ');
+    if(!localStorage.game) {
+        setGameToLocalStorage();
     } else {
-        el.className = el.className + ' ' + myClass;
+        game = JSON.parse(localStorage.game);
     }
-}
 
-document.addEventListener('DOMContentLoaded', function(){
-    var anchorLinks = document.querySelectorAll('.link_nav'),
-        menu = document.querySelector('.nav__links'),
-        hamburger = document.getElementById('hamburger');
+    $slider.slick({
+        accessibility: false,
+        arrows: false,
+        infinite: false,
+        fade: true,
+        initialSlide: getGameFromLocalStorage().current,
+    });
 
-    for (var i = 0; i < anchorLinks.length; i++) {
-        anchorLinks[i].addEventListener('click', function (e) {
-            e.preventDefault();
+    $('.js-start').on('click', function() {
+        $('.slider__input').each(function(i, el) {
+            if (!el.value.length) {
+                $(el).addClass('Error');
+                return false;
+            } else {
+                $(el).removeClass('Error');
+            }
 
-            toggleClass(hamburger, 'active');
-            toggleClass(menu, 'active');
+            if (i + 1 === $slider.find('.slider__input').length) {
+                game.name = $('.slider__input')[0].value.trim().substring(0, 32);
+                game.surname = $('.slider__input')[1].value.trim().substring(0, 32);
+                game.current = 1;
+                setGameToLocalStorage();
+                $slider.slick('slickNext');
+            }
+        });
+    });
 
-            var elementClick = this.getAttribute("href"),
-                destination = document.querySelector(elementClick).getBoundingClientRect().top + document.body.scrollTop;
+    $('.slider__input').on('input', function() {
+        $(this).removeClass('Error');
+    });
 
-            scroll(destination, 1500);
+    $('.js-button-correct, .js-button-wrong').on('click', function() {
+        var $this = $(this);
+        var questionIndex = $this.parents('.slider__item_question').index() - 1;
+        var result;
+        var imgUrl;
+        var text;
 
-            return false;
+        if($this.hasClass('js-button-correct')) {
+            result = true;
+            imgUrl = getGameFromLocalStorage().questions[questionIndex].imgCorrect;
+            text = getGameFromLocalStorage().questions[questionIndex].correctText;
+        } else {
+            result = false;
+            imgUrl = getGameFromLocalStorage().questions[questionIndex].imgWrong;
+            text = getGameFromLocalStorage().questions[questionIndex].wrongText;
+        }
+
+        $('.slider__item_answerresult').css("background-image", imgUrl);
+        $('.slider__item_answerresult .slider__title').html('<span>' + (questionIndex + 1) + ' вопрос</span>' + text);
+        game.questions[questionIndex].result = result;
+        game.current = $slider.slick('slickCurrentSlide') + 1;
+        setGameToLocalStorage();
+        $slider.slick('slickGoTo', 8,  false);
+    });
+
+    $('.js-button-next').on('click', function() {
+        var transform;
+
+        $slider.slick('slickGoTo', 9,  false);
+
+        switch(getGameFromLocalStorage().current) {
+            case 2:
+                transform = 'translate(125px, 40px)';
+                break;
+            case 3:
+                transform = 'translate(206px, 110px)';
+                break;
+            case 4:
+                transform = 'translate(140px, 180px)';
+                break;
+            case 5:
+                transform = 'translate(67px, 251px)';
+                break;
+            case 6:
+                transform = 'translate(110px, 331px)';
+                break;
+            case 7:
+                transform = 'translate(165px, 410px)';
+                break;
+        }
+
+        $('.marker').css('transition', 'all 1.5s ease').css('transform', transform);
+    });
+
+    $('.marker').on('transitionend webkitTransitionEnd oTransitionEnd', function(e) {
+        $slider.slick('slickGoTo', getGameFromLocalStorage().current,  false);
+    });
+
+
+
+    function setGameToLocalStorage() {
+        localStorage.setItem('game', JSON.stringify(game));
+
+        setMap();
+
+        console.log(getGameFromLocalStorage());
+    }
+
+    function getGameFromLocalStorage() {
+        return JSON.parse(localStorage.game);
+    }
+
+    function setMap() {
+        $.each(getGameFromLocalStorage().questions, function(i, el){
+            var $el = $('.map__button:nth-of-type(' + (i+1) + ')');
+
+            if (el.result === true) {
+                $el.addClass('correct');
+            } else if (el.result === false) {
+                $el.addClass('wrong');
+            }
         });
     }
-
-    hamburger.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        toggleClass(hamburger, 'active');
-        toggleClass(menu, 'active');
-    });
 });
